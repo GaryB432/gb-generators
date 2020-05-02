9d0979172d8041752ce615c295fca60f796fffd0
Gary Bortosky <garybort@gmail.com>
2020-05-02 12:53:51 -0500

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  extends: ["prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": 1,
+    "sort-keys": 2,
+    indent: 0,
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+};

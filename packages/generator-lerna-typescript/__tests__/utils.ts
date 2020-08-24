ebecbb7fa823c375bab684f793b8aa39a428de57
Gary Bortosky <garybort@gmail.com>
2020-08-24 14:56:07 -0500

    add tests

--- packages/generator-lerna-typescript/__tests__/utils.ts
+++ packages/generator-lerna-typescript/__tests__/utils.ts
@@ -1,7 +1,24 @@
 import utils = require("../utils");
 
 describe("Utilities", () => {
-  it("asdf", () => {
+  test("asdf", () => {
     expect(utils.getPackageInfo("asdf")).toEqual({ name: "asdf" });
+  });
+  test("with scope", () => {
+    expect(utils.getPackageInfo("@scope/asdf")).toEqual({
+      name: "asdf",
+      scope: "scope",
+    });
+  });
+  test("with camel scope", () => {
+    expect(utils.getPackageInfo("@MyScope/asdf")).toEqual({
+      name: "asdf",
+      scope: "my-scope",
+    });
+  });
+  test("two slashes", () => {
+    expect(utils.getPackageInfo("@scope/another/asdf")).toEqual({
+      name: "-scope-another-asdf",
+    });
   });
 });

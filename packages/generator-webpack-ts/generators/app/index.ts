d318928fb49ccb74e26e66b8f0fde0f233b9ab68
Gary Bortosky <garybort@gmail.com>
2020-12-30 01:03:28 -0600

    add manifest icons

--- packages/generator-webpack-ts/generators/app/index.ts
+++ packages/generator-webpack-ts/generators/app/index.ts
@@ -99,11 +99,15 @@ export default class extends Generator {
       this.destinationPath("src/public/index.html"),
       context
     );
-    this.fs.copyTpl(
-      this.templatePath("src/public/img/icon.svg.template"),
-      this.destinationPath("src/public/img/icon.svg"),
-      context
-    );
+    ["144", "512"]
+      .map((f) => `icon-${f}.png`)
+      .forEach((f) => {
+        this.fs.copyTpl(
+          this.templatePath(`src/public/img/${f}`),
+          this.destinationPath(`src/public/img/${f}`),
+          context
+        );
+      });
     this.fs.copy(
       this.templatePath("src/public/img/yeoman-003.png"),
       this.destinationPath("src/public/img/yeoman-003.png"),

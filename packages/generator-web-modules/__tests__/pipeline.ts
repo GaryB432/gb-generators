fe49089e0cefc7c0d7f853da30433298a25b20d9
Gary Bortosky <garybort@gmail.com>
2021-04-28 14:38:32 -0500

    add pipeline test

---
+++ packages/generator-web-modules/__tests__/pipeline.ts
@@ -0,0 +1,24 @@
+import path = require("path");
+import helpers = require("yeoman-test");
+
+describe("generator-web-modules:pipeline", () => {
+  describe("karma", () => {
+    let runResult: helpers.RunResult;
+    beforeEach(async () => {
+      runResult = await helpers
+        .create(path.join(__dirname, "../generators/pipeline"))
+        .run();
+    });
+    afterEach(() => {
+      if (runResult) {
+        runResult.restore();
+      }
+    });
+    it("runs correctly", () => {
+      runResult.assertFile([
+        ".github/workflows/main.yml",
+        "azure-pipelines.yml",
+      ]);
+    });
+  });
+});

e0d81ca777cde9c5ad299dbb71e59bef30bffe28
Gary Bortosky <garybort@gmail.com>
2021-05-01 09:40:19 -0500

    remove old subgenerator

--- packages/generator-web-modules/__tests__/pipeline.ts
+++ packages/generator-web-modules/__tests__/pipeline.ts
@@ -2,23 +2,18 @@ import path = require("path");
 import helpers = require("yeoman-test");
 
 describe("generator-web-modules:pipeline", () => {
-  describe("karma", () => {
-    let runResult: helpers.RunResult;
-    beforeEach(async () => {
-      runResult = await helpers
-        .create(path.join(__dirname, "../generators/pipeline"))
-        .run();
-    });
-    afterEach(() => {
-      if (runResult) {
-        runResult.restore();
-      }
-    });
-    it("runs correctly", () => {
-      runResult.assertFile([
-        ".github/workflows/main.yml",
-        "azure-pipelines.yml",
-      ]);
-    });
+  let runResult: helpers.RunResult;
+  beforeEach(async () => {
+    runResult = await helpers
+      .create(path.join(__dirname, "../generators/pipeline"))
+      .run();
+  });
+  afterEach(() => {
+    if (runResult) {
+      runResult.restore();
+    }
+  });
+  it("runs correctly", () => {
+    runResult.assertFile([".github/workflows/main.yml", "azure-pipelines.yml"]);
   });
 });
